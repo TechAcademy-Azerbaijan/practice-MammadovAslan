c85653cdc0329c9aa6d45ed0989965742d07229f
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-07-05 11:42:12 +0400

    task

--- task-8/main.js
+++ task-8/main.js
@@ -4,6 +4,13 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    const n = result.input.split(',')
+
+    if(n[0]*n[1]>0){
+        console.log(1);
+    }else if(n[0]*n[1]<0){
+        console.log(-1);
+    }else if(n[0]*n[1]==0){
+        console.log(0);
+    }
 });
