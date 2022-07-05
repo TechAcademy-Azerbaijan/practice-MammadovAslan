0efa736c365f964b602301d78d54611749ff57e8
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-07-05 11:11:13 +0400

    task

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,14 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    const n = result.input.split(',')
+    let arr = []
+    // for(let i = 0;i<n.length;i++){
+    //     console.log(n[i].split('').reverse().join(''));
+    // }
+
+    for(let i = n.length-1;i>=0;i--){
+        console.log(n[i].split('').join(''));
+    }
+
 });
