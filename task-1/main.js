e276c2c1056a6093afa05fbfe821371f6e48930a
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-07-05 10:38:31 +0400

    task

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,40 @@
-const prompt = require('prompt');
-prompt.start();
+// const prompt = require('prompt');
+// prompt.start();
 
 
-prompt.get('input', function (err, result) {
+// prompt.get('input', function (err, result) {
   
-    // Write code here
-  
-});
+
+//     const n = result.input
+
+//     n = n.toString().split('')
+
+//     console.log(n);
+
+// });
+
+//100 - 100
+//20 - 30
+//1 - 2
+
+let n = 123
+
+function getTotalPrice(n){
+    n = n.toString().split('').reverse()
+    let result = 0
+
+    for(let i = 1;i<=n.length;i++){
+        if(Number(n[i-1])*(10**i)/10 >= 100){
+            result += 100
+        }else if(Number(n[i-1])*(10**i)/10 >= 20 && Number(n[i-1])*(10**i)/10 <100){
+            result += 30
+        }else if(Number(n[i-1])*(10**i)/10 >= 1 && Number(n[i-1])*(10**i)/10 <20){
+            result += 2*(Number(n[i-1]))
+        }
+    }
+
+    return result
+}
+
+
+console.log(getTotalPrice(n));
