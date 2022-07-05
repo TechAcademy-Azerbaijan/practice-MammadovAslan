1396c02c5d47d5d125fa8968f5674685c9062e6d
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-07-05 11:36:19 +0400

    task

--- task-7/main.js
+++ task-7/main.js
@@ -7,22 +7,15 @@ prompt.get('input', function (err, result) {
 
     const n = result.input
 
-    function getSquareList(n){
         let arr = []
-        let newArr = []
         for(let i = 1;i<n;i++){
             arr.push(i)
         }
 
         for(let i = 0;i<arr.length;i++){
             if(Math.sqrt(arr[i])%1 ==0){
-                newArr.push(arr[i])
+                console.log(arr[i]);
             }
         }
-
-        return newArr
-    }
-
-    console.log(getSquareList(n));
 
 });
