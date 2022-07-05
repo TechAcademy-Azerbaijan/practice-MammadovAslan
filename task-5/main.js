fba5b8db0815e26591701bfd9a60bfc9c9fa3eb9
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-07-05 11:21:37 +0400

    task

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,19 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    const n = result.input.split(',')
+
+    function findMax(n){
+        let max = 0
+
+        for(let i = 0;i<n.length;i++){
+            if(parseInt(n[i]) >max){
+                max = parseInt(n[i])
+            }
+        }
+    return max
+    }
+
+    console.log(findMax(n));
+    
 });
